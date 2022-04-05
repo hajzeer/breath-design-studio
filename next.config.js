04250be7af6d846424e90e162e9a6e7c0fbdb49e
hajzeer <krzysh98@gmail.com>
2022-04-05 23:38:37 +0200

    contact form

--- next.config.js
+++ next.config.js
@@ -6,9 +6,9 @@
 const nextConfig = {
     reactStrictMode: true,
     env: {
-        PUBLIC_NEXT_MAIL,
-        PUBLIC_NEXT_PASS,
-        PUBLIC_NEXT_MAILTO
+        PUBLIC_NEXT_MAIL: process.env.PUBLIC_NEXT_MAIL,
+        PUBLIC_NEXT_PASS: process.env.PUBLIC_NEXT_PASS,
+        PUBLIC_NEXT_MAILTO: process.env.PUBLIC_NEXT_MAILTO
     }
 };
 
